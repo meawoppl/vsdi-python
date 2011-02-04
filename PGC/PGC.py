7fd5e5dedd0419b13faee9719a07bc3273948ecd
Matty G <meawoppl@gmail.com>
2011-02-04 17:50:59 -0600

    Added GPL liscense information

--- PGC/PGC.py
+++ PGC/PGC.py
@@ -2,13 +2,6 @@ from pylab import *
 from scipy import *
 from scipy import integrate
 
-# t  = linspace(0,10*pi,1000)
-# tc = sin(t)
-# tc = 1.0 * (tc > 0)
-
-# space_time_course = sig[:,:,newaxis] * tc
-
-
 def two_dee_normed_gaussian(sigma_in_px, size):
     assert len(size) == 2,  "Read the function label jerk"
     assert size[0] == size[1],  "Only square ATM"
@@ -24,18 +17,19 @@ def two_dee_normed_gaussian(sigma_in_px, size):
     return gauss
 
 # Make something semi-realistic
-times  = linspace(0,1.2, 132)
-on_off = (sin(times*2*pi) < 0.0) * 1.0
+times  = linspace(0,1.0, 1000)
+on_off = ((sin(times*10*pi) < 0.0) * 2.0) - 1.
 
-def dV_dt(V, t, I=on_off, It=times, g0=1, C=1, G_sigma=10, B_sigma=20):
+def dV_dt(V, t, I=on_off, It=times, g0=-1, C=1, G_sigma=10, B_sigma=20):
     ''' 
     This is the equation describing the population gain control model.
     This gets integrated by odeint to determine (n-dimensional) response 
     characters to a stimulus in time:
-      I is a 1d array describing the behavior of the signal in time It
+      I is a 1d array describing the contrast of the signal in time 
+      It is the time course that matches the above (units seconds)
       g0 is a constant related to the equalibrium condition
-      G_sigma is the standard devation of the cerebral PSF.
-      B_sigma is the standarrd deviation of the normalizing pool.
+      G_sigma is the standard deviation of the cerebral PSF.
+      B_sigma is the standard deviation of the normalizing pool.
     '''
 
     # ODEint expects a 1d vector, so (TODO: Ugleeee)
@@ -52,13 +46,19 @@ def dV_dt(V, t, I=on_off, It=times, g0=1, C=1, G_sigma=10, B_sigma=20):
     B = two_dee_normed_gaussian(B_sigma, V.shape)
 
     # Compute : C (dv/dt) = I*G + (g0(1+I*B)) V
-    dv_dt = ( (Ic*G) + (g0 * (1 + (Ic*B))) * V ) / C
+    dv_dt = ( (Ic*G) + (g0 * Ic * B * V ) )/ C
 
     # Squish it back to the vector
     return dv_dt.flatten()
 
+t  = linspace(0,10*pi,1000)
+tc = sin(t)
+tc = 1.0 * (tc > 0)
+
+orig_shape = (101, 101)
 
 times_of_interest = linspace(0.0, 1.0, 100)
-starting = zeros((101, 101)).flatten()
+starting = zeros(orig_shape).flatten()
 
-t = integrate.odeint(dV_dt, starting, times_of_interest)
+results = integrate.odeint(dV_dt, starting, times_of_interest)
+results = results.reshape(tuple([-1]) + orig_shape)
