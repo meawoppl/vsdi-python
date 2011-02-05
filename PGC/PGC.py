a2347d60e1a794fcc3d485835295f05801c0b7ed
Matty G <meawoppl@gmail.com>
2011-02-04 22:39:12 -0600

    PGC is working.  W00t.  Whoda thunk you only need about 20 lines of code. . . .

--- PGC/PGC.py
+++ PGC/PGC.py
@@ -18,9 +18,9 @@ def two_dee_normed_gaussian(sigma_in_px, size):
 
 # Make something semi-realistic
 times  = linspace(0,1.0, 1000)
-on_off = ((sin(times*10*pi) < 0.0) * 2.0) - 1.
+on_off = (sin(times*10*pi) < 0.0) * 1.0
 
-def dV_dt(V, t, I=on_off, It=times, g0=1, C=1, G_sigma=10, B_sigma=20):
+def dV_dt(V, t, I=on_off, It=times, g0=1, C=1, k=1e5, A_sigma=10, B_sigma=20):
     ''' 
     This is the equation describing the population gain control model.
     This gets integrated by odeint to determine (n-dimensional) response 
@@ -37,31 +37,62 @@ def dV_dt(V, t, I=on_off, It=times, g0=1, C=1, G_sigma=10, B_sigma=20):
     V = V.reshape((edge_width, edge_width))
 
     # Figure out if the stimulus is on or off, or transitioning
-    Ic = interp(t, I, It)
+    Ic = interp(t, It, I)
 
     # Make the smoothed stimulus 
-    G = two_dee_normed_gaussian(G_sigma, V.shape)
+    A = two_dee_normed_gaussian(A_sigma, V.shape) * Ic
 
     # Make the normalization pool gaussian
-    B = two_dee_normed_gaussian(B_sigma, V.shape)
+    B = two_dee_normed_gaussian(B_sigma, V.shape) * Ic * k
 
-    # Compute : C (dv/dt) = I*G + (g0(1+I*B)) V
-    dv_dt = ( (Ic*G) + (Ic * B * (g0 + V)  ) )/ C
+    # Relate this to RC circuit as the following
+    #    dV     V
+    # C ---- + ---  = 0
+    #    dt     R
+    #                      OR . . .
+    #    dV       /  V  \
+    #   ---- = - | ----  |
+    #    dt       \ RC  /
+
+    # Then we thing about conductance instead of resistance (1/R -> N)
+
+    #    dV       / NV  \
+    #   ---- = - | ----  |
+    #    dt       \  C  /
+
+    # But our conductor has a baseline and is a function local pooled 
+    # Properties as: 
+    # dv_dt =  10 * ((Ic + V_resting) - V)
+
+    dv_dt = A - (g0 * (1+B) * V)
 
     # Squish it back to the vector
     return dv_dt.flatten()
 
-t  = linspace(0,10*pi,1000)
-tc = sin(t)
-tc = 1.0 * (tc > 0)
-
 orig_shape = (101, 101)
+times_of_interest = linspace(0.0, 1.0, 1000)
+starting = zeros(orig_shape).flatten()
 
-times_of_interest = linspace(0.0, 1.0, 100)
-starting = ones(orig_shape).flatten()
-
-on_off[:] = 0
-on_off[500:750] = 0
-
-results = integrate.odeint(dV_dt, starting, times_of_interest)
+results = integrate.odeint(dV_dt, starting, times_of_interest, hmax=0.01)
 results = results.reshape(tuple([-1]) + orig_shape)
+
+close("all")
+
+figure()
+subplot(2,1,1)
+# Time Course of the stimulus
+
+plot(times, on_off)
+
+ylabel("Stimulus On-Off")
+axis((0.0, 1.0, 0.0, 1.2))
+
+subplot(2,1,2)
+# TC of the response
+plot(times_of_interest, results[:,50,50]*1000)
+
+xlabel("Time in Seconds")
+ylabel("Center Point Response")
+
+savefig("hells-yea.pdf")
+show()
