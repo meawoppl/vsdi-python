30ba3e594aff0626b3944a853722d1aee0f47762
Matty G <meawoppl@gmail.com>
2011-02-05 18:17:19 -0600

    Working well and looking nice

--- LM/fit_HB.py
+++ LM/fit_HB.py
@@ -1,7 +1,12 @@
+from scipy import *
+from scipy import optimize
+from warnings import warn
+from do_once import DoOnce
+
 def blank_fit_function(time_vector, a, b, c, tau, phi, hb_in_hz):
     return a * (exp(-time_vector/tau) - 1) + b * sin(2 * pi * hb_in_hz * time_vector - phi) + c
 
-def est_exp_decay(time_vector, mean_signal):
+def est_exp_decay(time_vector, mean_signal, debug=False):
     # TODO, docsting and assert docs
     assert mean_signal.shape == time_vector.shape
     # First_frame normalize the signal
@@ -17,13 +22,22 @@ def est_exp_decay(time_vector, mean_signal):
         # Sum of squared errors
         err = (mean_signal - expected_values)**2
 
-        # TODO: Debugging statement
-        print a, b, c, tau, phi, hb_in_hz, err.sum()
+        # # Handy Fit Debugging statement
+        if debug: print a, b, c, tau, phi, hb_in_hz, err.sum()
         return err.sum()
 
     # Starting guess . . . 
     c_guess = array([0.01,0, mean_signal.mean(), 1., 0., 2.])
-    return optimize.fmin(sum_sq_err, c_guess)
+
+    # Run the Simplex Optimizer
+    a, b, c, tau, phi, hb_in_hz = optimize.fmin(sum_sq_err, c_guess)
+
+    # Stupid check
+    assert (hb_in_hz < 4.0), "Heartbeat fit returned as > 4 Hz (%f)" % hb_in_hz
+    assert (hb_in_hz > 1.0), "Heartbeat fit returned as < 1 Hz (%f)" % hb_in_hz
+    
+    # Pass back found values
+    return a, b, c, tau, phi, hb_in_hz 
 
 
 @DoOnce
@@ -61,53 +75,64 @@ def compute_mean_centerblock_conditions(hdf5_filename):
     
     return cn_to_meantrace
 
-if __name__ == "__main__":
-    # Now lets average the exp. falloff of our dataset.  
-    import tables
-    from pylab import *
-    
-    # Laptop
-    cn_to_tc = compute_mean_centerblock_conditions("/home/meawoppl/Frodo20100716-run0.h5", do_debug=True)
 
-    # # Office
-    # cn_to_tc = compute_mean_centerblock_conditions("/media/Local Disk/Frodo20100716-run0.h5")
+def graph_hb_trend(h5_file, img_file):
+    # Open the hdf5 file and get the centerblock trends
+    cn_to_tc = compute_mean_centerblock_conditions(h5_file, do_debug=True)
 
+    # TODO!  Unhardcode this!
     exp_times = linspace(0, 1.2, 132)
-    lmd = LM_Decomposer(exp_times)
-
-    # # This is just like from the paper to confirm that the
-    # # SVD vector extraction is working as expected
-    # a_mins  = array([20,  30,  450, 60], dtype=float64)
-    # a_maxs  = array([120, 200, 590, 100], dtype=float64)
-    # a_steps = array([11,  18, 15, 4], dtype=int32)
-    # lmd.add_signal_group(a_mins, a_maxs, a_steps)
 
     figure()
     for cn, ntr in cn_to_tc.iteritems():
-        plot(exp_times, ntr, label=str(cn), alpha=0.8)
+        # If it is the blank condition
+        if cn == 0: 
+            # Fit the mean blank with exp sinusoidy thing (get the paramaters)
+            a, b, c, tau, phi, hb_in_hz = est_exp_decay(exp_times, ntr)
 
-        if cn == 0:
-            parms = est_exp_decay(exp_times, ntr)
-            a, b, c, tau, phi, hb_in_hz = parms
-            # hb_in_hz = 2.
-
+            # Compute the fit shape from those paramaters
             fit_vals = blank_fit_function(exp_times, a, b, c, tau, phi, hb_in_hz)
+            
+            # Plot the fit, in bark black with a dashed link
             plot(exp_times, fit_vals, "black", linestyle="--", alpha=0.9,  label="Blank Fit")
 
+            # Make the blank conditions stand out
+            plot_alpha = 1.0
+        else:
+            # But still plot the non-blank
+            plot_alpha = 0.3
+
+        # This plots the data for both the above with different alpha values
+        plot(exp_times, ntr, label="Cond. "+str(cn), alpha=plot_alpha)
+
     legend()
+
+    # Print out the fit equation on the plot
     equation_text = r"FIT = $a \, e^{(-t/\tau) - 1} + b \, \sin(2 \pi f \, t + \phi) +c$"
 
+    # These describe the varible values used
     var_text1 = r"$a=%.03E \,\,\,\, \tau=%.03f \,\,\,\,  b=%.03E$" % (a, tau, b)
     var_text2 = r"$f=%.03f \,\,\,\, \phi=%.03E \,\,\,\,  c=%.03E$" % (hb_in_hz, phi, c)
+
+    # Lower left cornerish
     text(0.05, -0.0045, equation_text)
     text(0.05,-0.0047, var_text1)
     text(0.05,-0.0049, var_text2)
 
+    # Title and axis labels
     title("Center Block Mean per Condiiton (Frodo20100716-run0)")
-
     xlabel("Time in Seconds")
     ylabel("$\Delta f/f$")
 
-    savefig("blank-fitting-params.pdf")
+    savefig(img_file)
 
-    show()
+
+if __name__ == "__main__":
+    # Now lets average the exp. falloff of our dataset.  
+    import tables, sys
+
+    # Stupid check the CLI information
+    assert len(sys.argv) == 3, "\nUsage: python %s [hdf5-file] [output-image]" % sys.argv[0]
+    from pylab import *
+
+    graph_hb_trend(sys.argv[1], sys.argv[2])
